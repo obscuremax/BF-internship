3504dc8378cb7dabe40f102705e1e5c5e54e9cc0
Maksim Voronovich <maxxam@yandex.ru>
2021-09-10 01:10:06 +0300

    add block scenario

--- src/js/script.js
+++ src/js/script.js
@@ -4,11 +4,11 @@ const sphereSlider = new Swiper('.sphere-slider', {
   slidesPerGroup: 2,
   spaceBetween: 30,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.sphere-slider__button-next',
+    prevEl: '.sphere-slider__button-prev',
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: '.sphere-slider__pagination',
     type: 'bullets',
     clickable: true,
   },
@@ -20,6 +20,42 @@ const sphereSlider = new Swiper('.sphere-slider', {
       spaceBetween: 0,
     },
     769: {
+      slidesPerView: 1,
+      slidesPerColumn: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 0,
+    },
+  },
+});
+
+const scenarioSlider = new Swiper('.scenario-slider', {
+  slidesPerView: 1,
+  slidesPerColumn: 2,
+  slidesPerGroup: 1,
+  spaceBetween: 75,
+  navigation: {
+    nextEl: '.scenario-slider__button-next',
+    prevEl: '.scenario-slider__button-prev',
+  },
+  pagination: {
+    el: '.scenario-slider__pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      slidesPerColumn: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 0,
+    },
+    769: {
+      slidesPerView: 1,
+      slidesPerColumn: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 0,
+    },
+    1024: {
       slidesPerView: 1,
       slidesPerColumn: 1,
       slidesPerGroup: 1,
