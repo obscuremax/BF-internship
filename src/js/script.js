3b66cceaf903aa4f40f4ebbb9b665b7ec3494ba9
Maksim Voronovich <maxxam@yandex.ru>
2021-09-14 01:46:52 +0300

    add block choice and listener for buttons and input

--- src/js/script.js
+++ src/js/script.js
@@ -81,3 +81,111 @@ const cashSlider = new Swiper('.cash-slider', {
   loop: true,
 });
 
+const label = document.querySelector('.label--add');
+
+document.addEventListener('click', (e) => {
+  if (e.target.closest('#price-range')) {
+    return;
+  }
+  const cashCard = document.querySelector('.cash-card');
+  const cashSpecifications = document.querySelector('.cash-specifications');
+  const labelCross = e.target.closest('.label__cross');
+  const btnAddLabel = e.target.closest('.btn-add');
+
+  const tabsItems = e.target.parentElement.closest('.tabs');
+  const tabsItem = e.target.parentElement.closest('.tabs__item');
+
+  if (labelCross) {
+    deleteLabel(labelCross);
+  }
+  if (btnAddLabel) {
+    addLabel(btnAddLabel);
+  }
+  if (!tabsItems) {
+    return;
+  } else {
+    removeClassListActiveFromTabs(tabsItems.children);
+  }
+  if (tabsItem) {
+    addClassList(tabsItem, 'tabs__item--active');
+    if (tabsItem.children[0].innerHTML == 'Характеристики') {
+      removeClassList(cashCard, 'cash-card--active');
+      addClassList(cashSpecifications, 'cash-specifications--active');
+    }
+    if (tabsItem.children[0].innerHTML == 'О&nbsp;кассе') {
+      addClassList(cashCard, 'cash-card--active');
+      removeClassList(cashSpecifications, 'cash-specifications--active');
+    }
+    if (
+      tabsItem.children[0].innerHTML == 'Купить' ||
+      tabsItem.children[0].innerHTML == 'Аренда' ||
+      tabsItem.children[0].innerHTML == 'Наше&nbsp;ПО'
+    ) {
+      constructor(tabsItem);
+    }
+  }
+});
+
+document.addEventListener('input', (e) => {
+  const inputRange = e.target.closest('#price-range');
+  const inputPrice = document.getElementById('price');
+  inputPrice.setAttribute('value', inputRange.value + ' 000 руб/мес');
+});
+
+function deleteLabel(el) {
+  el.parentElement.outerHTML = '';
+}
+function addLabel(el) {
+  el.insertAdjacentHTML('beforebegin', label.outerHTML);
+}
+
+function removeClassListActiveFromTabs(array) {
+  for (const el of array) {
+    removeClassList(el, 'tabs__item--active');
+  }
+}
+function addClassList(el, classEl) {
+  el.classList.add(classEl);
+}
+function removeClassList(el, classEl) {
+  el.classList.remove(classEl);
+}
+
+function constructor(el) {
+  const constructortext = el.dataset.constructortext;
+  const blocktext = el.dataset.blocktext;
+  const blockprice = el.dataset.blockprice;
+  const labelname = el.dataset.labelname;
+  const option = el.dataset.option;
+
+  const firstLabel = document.querySelector('.constructor__scenario')
+    .children[0];
+  const options = Array.from(
+    document.querySelector('.constructor__select').children
+  );
+  const select = document.querySelector('.constructor__select');
+  const constructorText = document.querySelector('.constructor__text');
+  const blockText = document.querySelectorAll('.choice-card__block')[2]
+    .children[0];
+  const blockPrice = document.querySelectorAll('.choice-card__block')[2]
+    .children[1];
+
+  firstLabel.textContent = labelname;
+  select.innerHTML = '';
+
+  options.forEach((el, i) => {
+    if (i == 0) {
+      el.value = option;
+      el.innerHTML = option;
+      select.innerHTML = el.outerHTML;
+    } else {
+      el.value = option + `${i++}`;
+      el.innerHTML = option + `${i++}`;
+      select.innerHTML += el.outerHTML;
+    }
+  });
+  firstLabel.append(select);
+  constructorText.innerHTML = constructortext;
+  blockText.innerHTML = blocktext;
+  blockPrice.innerHTML = blockprice;
+}
